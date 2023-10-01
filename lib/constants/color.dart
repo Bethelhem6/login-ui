bab2bddb03b376dba8ec7629d1543bafc102cacc
Bethelhem Misgina <bettymisg6@gmail.com>
2023-10-01 17:42:35 +0300

    define constants

---
+++ lib/constants/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundColor = Colors.white;
+  static Color get primaryText => const Color(0xff030303);
+  static Color get secondaryText => const Color(0xff828282);
+  static Color get ligtGray => const Color(0xffB1B1B1);
+  static Color get darkGray => const Color(0xff4C4F4D);  static const color = Colors.white;
+}
---
+++ lib/constants/font.dart
@@ -0,0 +1,5 @@
+class AppFont {
+  static double get bigFont => 16;
+  static double get smallText => 12;
+  static double get mediumText => 14;
+}
